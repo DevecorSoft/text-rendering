c63fb5cb5eac291751f9554f2712ed4aaf8da110
Devecor <devecor@outlook.com>
2022-06-23 14:41:18 +0800

    initialize converting(backend)

---
+++ converting/src/main/kotlin/cn/devecorsoft/converting/ConvertingApplication.kt
@@ -0,0 +1,11 @@
+package cn.devecorsoft.converting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConvertingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConvertingApplication>(*args)
+}
---
+++ converting/src/test/kotlin/cn/devecorsoft/converting/ConvertingApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.devecorsoft.converting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConvertingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
