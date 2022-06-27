d4a918fdef8e501f1a40097a5afe693addf710ad
Devecor <devecor@outlook.com>
2022-06-27 18:43:29 +0800

    send markdown payload to server

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import "./App.css"
-import { Layout, Space } from "antd"
+import { Layout, Space, Button } from "antd"
 import ForkMeOnGithub from "./components/fork-me-on-github/fork-me-on-github"
 import Editor from "@monaco-editor/react"
+import monaco from "monaco-editor/esm/vs/editor/editor.api"
+import { useRef } from "react"
+import axios from "axios"
 
 const { Header, Content } = Layout
 
 function App() {
+    const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null)
+
+    function handleEditorDidMount(editor: monaco.editor.IStandaloneCodeEditor) {
+        editorRef.current = editor
+    }
+
+    function render() {
+        axios
+            .post("http://localhost:8080/rendering/markdown", {
+                content: editorRef.current?.getValue(),
+            })
+            .then((data) => {
+                console.log(data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
     return (
         <Layout>
             <ForkMeOnGithub />
@@ -23,12 +45,18 @@ function App() {
                     <div>language selector</div>
                     <div className="ptr-container">
                         <Editor
-                            height="80vh"
-                            defaultLanguage="javascript"
-                            defaultValue="// some comment"
+                            height="70vh"
+                            defaultLanguage="markdown"
+                            defaultValue={"# Title\ndescription\n"}
+                            onMount={handleEditorDidMount}
                         />
+                        <Button type="primary" onClick={render}>
+                            Render
+                        </Button>
                     </div>
-                    <div className="ptr-container">previewer container</div>
+                    <div className="ptr-container">
+                        <div id="container">previewer container</div>
+                    </div>
                     <div>menu area</div>
                 </Space>
             </Content>
