dc9c572e4010fb1b44b0b69ce31b4977b4caefe1
Devecor <devecor@outlook.com>
2022-06-28 21:22:47 +0800

    enable cors

--- converting/src/main/kotlin/cn/devecorsoft/converting/RoutingConfig.kt
+++ converting/src/main/kotlin/cn/devecorsoft/converting/RoutingConfig.kt
@@ -2,6 +2,9 @@ package cn.devecorsoft.converting
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 import org.springframework.web.reactive.function.server.*
 
 @Configuration
@@ -9,5 +12,20 @@ class RoutingConfig {
     @Bean
     fun monoRouting(renderingMarkdownHandler: RenderingMarkdownHandler): RouterFunction<ServerResponse> = coRouter {
         POST(Endpoints.RENDER_MARKDOWN, renderingMarkdownHandler::render)
+    }
+
+    @Bean
+    fun corsFilter(): CorsWebFilter {
+        val config = CorsConfiguration()
+
+        config.allowCredentials = true
+        config.addAllowedOriginPattern("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration(Endpoints.RENDER_MARKDOWN, config)
+        }
+        return CorsWebFilter(source)
     }
 }
