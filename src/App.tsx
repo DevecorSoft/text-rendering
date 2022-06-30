f4c67e431089c2df0aae8082c107d695d7f29636
Devecor <devecor@outlook.com>
2022-06-30 23:28:48 +0800

    refine preview and render in front end

--- src/App.tsx
+++ src/App.tsx
@@ -21,24 +21,40 @@ function App() {
 
     function render() {
         axios
-            .post(
-                "http://localhost:8080/rendering/markdown",
-                {
-                    content: editorRef.current?.getValue(),
-                },
-                { responseType: "arraybuffer" }
-            )
-            .then((response) => {
-                renderAsync(
-                    response.data,
-                    document.getElementById("container")!
-                ).then(() => {
-                    console.log("docx: finished!")
-                })
+        .post(
+            "http://localhost:8080/rendering/markdown/file",
+            {
+                content: editorRef.current?.getValue(),
+            }
+        )
+        .then((response) => {
+            location.replace(`http://localhost:8080/docx${response.data}`)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    function preview() {
+        axios
+        .post(
+            "http://localhost:8080/rendering/markdown",
+            {
+                content: editorRef.current?.getValue(),
+            },
+            { responseType: "arraybuffer" }
+        )
+        .then((response) => {
+            renderAsync(
+                response.data,
+                document.getElementById("container")!
+            ).then(() => {
+                console.log("docx: finished!")
             })
-            .catch((err) => {
-                console.log(err)
-            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
     }
 
     return (
@@ -63,6 +79,9 @@ function App() {
                             defaultValue={"# Title\ndescription\n"}
                             onMount={handleEditorDidMount}
                         />
+                        <Button type="primary" onClick={preview}>
+                            Preview
+                        </Button>
                         <Button type="primary" onClick={render}>
                             Render
                         </Button>
